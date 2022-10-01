35231dc3dea654e7fe31295b4b4f7320781ea203
alvintran888 <dikhapmuonnoi0703@gmail.com>
2022-10-01 17:28:05 +0700

    mongo

--- app.js
+++ app.js
@@ -7,39 +7,80 @@ const mongoose = require("mongoose");
 const studentModel = require("./models/KakalotSchema");
 const kakalotModel = require("./models/KakalotSchema");
 const url = "mongodb://localhost:27017/xayda";
-mongoose.connect(url, { useNewUrlParser: true }, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("connect to db succeed !");
-  }
+mongoose.connect(url, {
+    useNewUrlParser: true
+}, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("connect to db succeed !");
+    }
+});
+
+//body-parser: get form input
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//render ra form add
+app.get("/add", (req, res) => {
+    res.render("add");
+});
+
+//nhận và xử lý dữ liệu từ form add
+app.post("/add", (req, res) => {
+    //   console.log(req.body);
+    // res.send(req.body);
+    res.render("output", {
+        kakalot: req.body
+    });
 });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
 });
 
 app.get("/about", function (req, res) {
-  res.render("about");
+    res.render("about");
 });
 
-app.get("/kakalot", (req, res) => {
-  //SQL : select * from student
-  kakalotModel.find((err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      //1. show dữ liệu ra console log
-      //console.log(data);
-      //2. show dữ liệu ra API bằng "send"
-      //res.send(data);
-      //3. show dữ liệu ra view bằng "render"
-      res.render("kakalot", { kakalots: data });
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    var check = "Login failed !";
+    if (username == "admin" && password == "123456") {
+        check = "Login succeed !";
     }
-  });
+    //res.send(check);
+    res.render("check", {
+        result: check
+    });
+}); 
+
+app.get("/kakalot", (req, res) => {
+    //SQL : select * from student
+    kakalotModel.find((err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            //1. show dữ liệu ra console log
+            //console.log(data);
+            //2. show dữ liệu ra API bằng "send"
+            //res.send(data);
+            //3. show dữ liệu ra view bằng "render"
+            res.render("kakalot", {
+                kakalots: data
+            });
+        }
+    });
 });
 
 app.listen(port);
